01ca1718c3a281db1933553962fd2bec9394a968
Paul Brennan <brennanpincardiff@gmail.com>
2015-12-04 17:13:04 +0000

    Reformatting graph

--- Med_Schools_Council_data_PDFs/BenchmarkingDataClinical20151204.R
+++ Med_Schools_Council_data_PDFs/BenchmarkingDataClinical20151204.R
@@ -17,11 +17,22 @@ p <- ggplot(data=data[1:18,],
        aes(x = year, 
            y = percentF,
            colour = title)) +
-      geom_point() +
-      geom_line() +
-      ylab("Clinical staff by grade (Percentage Female)") +
-      xlab("Year") +
+      geom_point(size = 3) +
+      geom_line(size = 1) +
+      ylab("Clinical staff by grade (Percentage Female)") + 
+      xlab("") +
       ylim(0,50) +
-      ggtitle("Benchmarking data - Clinical Staff in UK \n Source: Medical Schools Council") +
       theme_bw()
-p
+
+# y-axis label and text looks too small. 
+p <- p + theme(axis.text = element_text(size = 14, face = "bold")) +
+         theme(axis.title = element_text(size = 16, face = "bold"))
+
+# reposition the legend
+p <- p + theme(legend.position = c(1,0),     # moves legend to bottom right
+               legend.justification = c(1,-0.05),  # moves it in a bit 
+               legend.text=element_text(size = 12),  # larger text
+               legend.title=element_text(size = 12))
+
+p + labs(title = "Clinical Staff in UK \n Source: Medical Schools Council") +
+    theme(plot.title = element_text(size = 18, face = "bold"))
